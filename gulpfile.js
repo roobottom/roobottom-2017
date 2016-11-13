518a5c743afb4ea6d6bd40f0e5f9e53cda6cefd4
Jon Roobottom <roo@roodesign.co.uk>
2016-11-13 22:55:50 +0000

    posts are now processed via gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,27 @@
-var gulp = require('gulp');
-var $ = require('gulp-load-plugins')();
+'use strict';
 
-// gulp.task('jekyll', function (gulpCallBack){
-//   var spawn = require('child_process').spawn;
-//   var jekyll = spawn('jekyll', ['serve'], {stdio: 'inherit'});
-//   jekyll.on('exit', function(code) {
-//     gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: ' + code);
-//   });
-// });
+const gulp = require('gulp');
+const through = require('through2');
+const nunjucks = require('nunjucks');
+const path = require('path');
+const del = require('del');
+
+const $ = require('gulp-load-plugins')({
+  rename: {
+    'gulp-front-matter': 'fm'
+  }
+});
+
+const opts = {
+  publish_folder: './docs',
+  categories: [
+    {
+      name: 'articles',
+      source: './_source/posts/articles/*.md',
+      template: './_source/templates/article.html'
+    }
+  ]
+}
 
 //testing web server
 gulp.task('test_server',() => {
@@ -19,12 +33,41 @@ gulp.task('test_server',() => {
   })
 });
 
+function applyTemplate(templateFile) {
+
+  return through.obj(function (file, enc, cb) {
+      var data = {
+          page: file.page,
+          content: file.contents.toString()
+      };
+      file.contents = new Buffer(nunjucks.render(path.join(__dirname, templateFile),data), 'utf8');
+      this.push(file);
+      cb();
+  });
+}
+
 //HTML
 gulp.task('html',() => {
     gulp.src('./_source/pages/**/*.html')
     .pipe($.nunjucks.compile())
     .pipe($.connect.reload())
+    .pipe(gulp.dest(opts.publish_folder))
+});
+
+//Posts
+gulp.task('posts',() => {
+
+  opts['categories'].map((category) => {
+    console.log('processing posts in ',category.name);
+
+    gulp.src(category.source)
+    .pipe($.fm({property: 'page', remove: true}))
+    .pipe($.marked())
+    .pipe(applyTemplate(category.template))
     .pipe(gulp.dest('./docs'))
+
+  });
+
 });
 
 //wachers
