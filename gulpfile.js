fb30f46abecca2fd0d903c7465b2d079bc29bb37
Jon Roobottom <roo@roodesign.co.uk>
2016-11-20 14:36:24 +0000

    totally updated gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,12 @@
 const gulp = require('gulp');
 const through = require('through2');
 const nunjucks = require('nunjucks');
+const nunjucks_env = new nunjucks.Environment(new nunjucks.FileSystemLoader('./_source'));
+
 const path = require('path');
 const del = require('del');
 const fs = require('fs');
-
-const nunjucks_env = new nunjucks.Environment(new nunjucks.FileSystemLoader('./_source'));
+const gutil = require('gulp-util');
 
 const $ = require('gulp-load-plugins')({
   rename: {
@@ -28,7 +29,6 @@ const site = {
     }
   ]
 }
-
 const opts = {
   publish_folder: './docs',
   pages_folder: './_source/pages/**/*',
@@ -41,92 +41,124 @@ const opts = {
   ]
 }
 
-//testing web server
-gulp.task('test_server',() => {
-  $.connect.server({
-    root: './docs',
-    port: 8020,
-    name: 'Test Server',
-    livereload: true
-  })
+gulp.task('clean',() => {
+  return del([opts.publish_folder]);
 });
 
-//static pages
-gulp.task('html:pages',() => {
-  gulp.src(opts.pages_folder)
-  .pipe($.if(src => {
-    if (path.basename(src.path) != 'index.html')
-      return true;
-  }, $.rename(src => {
-    src.dirname = src.basename + '/';
-    src.basename = 'index'
-  })))
-  .pipe(renderPage())
-  .pipe(gulp.dest(opts.publish_folder))
-});
+/*
+render posts
+*/
 
-//posts
-gulp.task('html:posts',() => {
-
-  opts['categories'].map((category) => {
-    gulp.src(category.source)
+gulp.task('posts', () => {
+  return gulp.src('./_source/posts/**/*.md')
     .pipe($.fm({property: 'page', remove: true}))
     .pipe($.marked())
-    .pipe(renderPost(category.template))
+    .pipe(updatePostsObject())
+    .pipe(renderFileWithTemplate('./_source/templates/article.html'))
     .pipe($.rename((src)=> {
-      src.dirname = category.name + '/' + src.basename + '/';
+      src.dirname = 'articles/' + src.basename + '/';
       src.basename = 'index'
     }))
-    .pipe($.connect.reload())
     .pipe(gulp.dest(opts.publish_folder))
-
-  });
 });
 
-//generate pages
-gulp.task('html',['clean:html','html:pages','html:posts']);
-
-//cleanup
-gulp.task('clean:html',() => {
-  return del([opts.publish_folder + '/**/*.html']);
+/*
+render archive pages
+*/
+gulp.task('archives', ['posts'], () => {
+  return posts()
+  .pipe(renderFileWithTemplate('./_source/pages/articles.html'))
+  .pipe(gulp.dest('./docs'))
 });
 
-//wachers
-gulp.task('watch',() => {
-  gulp.watch(['./_source/**/*.html'],['reload'])
-});
-
-//runners
-gulp.task('default',['test_server','html','watch']);
-gulp.task('reload',['html']);
-
-
-//---
-//custom functions
-//---
-
-function renderPost(templateFile) {
+var updatePostsObject = () => {
+  let posts = [];
   return through.obj(function (file, enc, cb) {
-      let data = {
-          site: site,
-          page: file.page,
-          content: file.contents.toString()
-      };
-      var wrapper = fs.readFileSync(path.join(__dirname, templateFile),{encoding:'utf8'});
-      file.contents = new Buffer(nunjucks_env.renderString(wrapper,data), 'utf8');
-      this.push(file);
-      cb();
+    posts.push(file.page);
+    this.push(file);
+    cb();
+  },
+  (cb) => {
+    posts.sort((a,b) => {
+        return b.date - a.date;
+    });
+    for (let key in posts) {
+      console.log(posts[key].title)
+    }
+    console.log('bye!');
+    site.posts = posts;
+    cb();
   });
 }
 
-function renderPage() {
+var renderFileWithTemplate = (templateFile) => {
   return through.obj(function (file, enc, cb) {
-    let orriginal = file.contents.toString();
     let data = {
-      site: site
-    }
-    file.contents = new Buffer(nunjucks_env.renderString(orriginal,data), 'utf8');
+        site: site,
+        page: file.page,
+        content: file.contents.toString()
+    };
+    //choosing to open the file manually, and render the template as a string
+    //as nunjucks.render caches the file its passed, and the watch function doesn't work.
+    var wrapper = fs.readFileSync(path.join(__dirname, templateFile),{encoding:'utf8'});
+    file.contents = new Buffer(nunjucks_env.renderString(wrapper,data), 'utf8');
     this.push(file);
     cb();
   });
+};
+
+function posts(basename, count) {
+  console.log('---------------- running posts! -----------');
+  basename = 'artiles';
+  count = 10;
+  var stream = through.obj(function(file, enc, cb) {
+		this.push(file);
+		cb();
+	});
+  //console.log(site.posts);
+  if (site.posts)
+  {
+    var c     = 0;
+    var page  = 0;
+    var posts = [];
+    site.posts.forEach(function (post) {
+      posts.push(post);
+      c++;
+      if (c == count) {
+        var file = new gutil.File({
+          path: basename + (page == 0 ? '' : page) + '.html',
+          contents: new Buffer('')
+        });
+        //console.log('page=' + page + ' c=' + c + ' posts.length=' + site.posts.length);
+        file.page = {
+          posts: posts,
+          prevPage: page != 0 ? basename + ((page-1) == 0 ? '' : page-1) + '.html' : null,
+          nextPage: (page+1) * count < site.posts.length ? basename + (page+1) + '.html' : null,
+          };
+        stream.write(file);
+
+        c = 0;
+        posts = [];
+        page++;
+      }
+    });
+
+    if (posts.length != 0) {
+      var file = new gutil.File({
+        path: basename + (page == 0 ? '' : page) + '.html',
+        contents: new Buffer('')
+      });
+      file.page = {
+        posts: posts,
+        prevPage: page != 0 ? basename + ((page-1) == 0 ? '' : page) + '.html' : null,
+        nextPage: null,
+        };
+      stream.write(file);
+    }
+  }
+
+  stream.end();
+  stream.emit("end");
+
+  return stream;
 }
