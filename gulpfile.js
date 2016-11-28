ae7206e39b5bd1446f64d0e912a306129ae1e208
Jon Roobottom <roo@roodesign.co.uk>
2016-11-28 19:53:47 +0000

    updatePostsObject is now a module

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,8 @@ const fs = require('fs');
 const gutil = require('gulp-util');
 const _ = require('lodash');
 
+const updatePostsObject = require('./modules_backend/updatePostsObject');
+
 const $ = require('gulp-load-plugins')({
   rename: {
     'gulp-front-matter': 'fm'
@@ -66,7 +68,7 @@ process all posts
 gulp.task('posts:process', () => {
   return gulp.src('./_source/posts/**/*.md')
     .pipe($.fm({property: 'page', remove: true}))
-    .pipe(updatePostsObject())
+    .pipe(updatePostsObject(site))
 });
 
 /*
@@ -95,44 +97,7 @@ gulp.task('archives', ['posts'], () => {
   .pipe(gulp.dest('./docs'))
 });
 
-var updatePostsObject = () => {
-  let posts = [];
-  return through.obj(function (file, enc, cb) {
-    let fileobj = path.parse(file.path);
-    file.page.id = fileobj.name;
-    file.page.category = fileobj.dir.split('/').slice(-1)[0];
-    posts.push(file.page);
-    this.push(file);
-    cb();
-  },
-  (cb) => {
-    posts.sort((a,b) => {
-        return b.date - a.date;
-    });
-    for (let key in posts) {
-      let next = parseInt(key)-1;
-      let prev = parseInt(key)+1;
-      //console.log(posts[key].title);
-      if(next in posts) {
-        posts[key].next = {};
-        posts[key].next.title = posts[next].title;
-        posts[key].next.id = posts[next].id;
-        posts[key].next.date = posts[next].date;
-        posts[key].next.category = posts[next].category;
-      }
-      if(prev in posts) {
-        posts[key].prev = {};
-        posts[key].prev.title = posts[prev].title;
-        posts[key].prev.id = posts[prev].id;
-        posts[key].prev.date = posts[prev].date;
-        posts[key].prev.category = posts[prev].category;
-      }
-    };
-    //console.log('bye!');
-    site.posts = posts;
-    cb();
-  });
-}
+
 
 var renderFileWithTemplate = (templateFile) => {
   return through.obj(function (file, enc, cb) {
---
+++ modules_backend/updatePostsObject.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const path = require('path');
+const through = require('through2');
+
+module.exports = function(site) {
+  let posts = [];
+  return through.obj(function (file, enc, cb) {
+    let fileobj = path.parse(file.path);
+    file.page.id = fileobj.name;
+    file.page.category = fileobj.dir.split('/').slice(-1)[0];
+    posts.push(file.page);
+    this.push(file);
+    cb();
+  },
+  function(cb) {
+    posts.sort((a,b) => {
+        return b.date - a.date;
+    });
+    for (let key in posts) {
+      let next = parseInt(key)-1;
+      let prev = parseInt(key)+1;
+      //console.log(posts[key].title);
+      if(next in posts) {
+        posts[key].next = {};
+        posts[key].next.title = posts[next].title;
+        posts[key].next.id = posts[next].id;
+        posts[key].next.date = posts[next].date;
+        posts[key].next.category = posts[next].category;
+      }
+      if(prev in posts) {
+        posts[key].prev = {};
+        posts[key].prev.title = posts[prev].title;
+        posts[key].prev.id = posts[prev].id;
+        posts[key].prev.date = posts[prev].date;
+        posts[key].prev.category = posts[prev].category;
+      }
+    };
+    site.posts = posts;
+    cb();
+  });
+}
