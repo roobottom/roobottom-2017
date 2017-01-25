b5206045d7229f615056b3167d1fbb6949399f5f
roobottom <roo@roodesign.co.uk>
2017-01-25 15:27:46 +0000

    updated navigation

--- site.js
+++ site.js
@@ -6,17 +6,32 @@ module.exports = {
     {
       url: '/',
       title: 'Home',
-      icon: 'house'
+      icon: 'house',
+      prompt: 'Go to the homepage'
     },
     {
       url: '/articles',
       title: 'Articles',
-      icon: 'teacup'
+      icon: 'teacup',
+      prompt: 'Read things I\'ve written'
     },
     {
       url: '/patterns',
       title: 'Pattern library',
-      icon: 'clipboard'
+      icon: 'clipboard',
+      prompt: 'See how this site is built'
+    }
+  ],
+  social: [
+    {
+      url: 'https://github.com/roobottom',
+      title: 'Github',
+      icon: 'github'
+    },
+    {
+      url: 'https://twitter.com/roobottom',
+      title: 'Twitter',
+      icon: 'twitter'
     }
   ],
   articles: {
