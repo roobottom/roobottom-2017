f53493162271b8811b7a27e2d6abd06ea169b11e
roobottom <roo@roodesign.co.uk>
2018-01-30 15:55:46 +0000

    changed tracking code

--- _source/load-later.js
+++ _source/load-later.js
@@ -3,20 +3,18 @@ const m_switcher = require('./patterns/modules/m_switcher/m_switcher.js');
 const m_figure = require('./patterns/modules/m_figure/m_figure.js');
 const utils = require('./patterns/_shared/utils.js');
 
+var clicky_site_ids = [101098446];
+
 (function(w, d, undefined){
   c_fullscreen(w,d);
   m_switcher(w,d);
   m_figure(w,d);
 
-  //gauges tracking script
-  var _gauges = _gauges || [];
-  var t   = d.createElement('script');
-  t.type  = 'text/javascript';
-  t.async = true;
-  t.id    = 'gauges-tracker';
-  t.setAttribute('data-site-id', '5a55f8e56eb5fb659f003fa9');
-  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif');
-  t.src = 'https://d2fuc4clr7gvcn.cloudfront.net/track.js';
-  var s = d.getElementsByTagName('script')[0];
-  s.parentNode.insertBefore(t, s);
+  //clicky tracking script
+  var s = d.createElement('script');
+  s.type = 'text/javascript';
+  s.async = true;
+  s.src = '//static.getclicky.com/js';
+  ( d.getElementsByTagName('head')[0] || d.getElementsByTagName('body')[0] ).appendChild(s);
+
 }(window,document));
